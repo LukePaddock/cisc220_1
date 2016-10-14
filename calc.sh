115b6b87209700d22bd91b31ab4e72fad1d61e50
LukePaddock <14lp9@queensu.ca>
2016-10-14 16:51:58 -0400

    Finished the first program!

--- calc.sh
+++ calc.sh
@@ -8,17 +8,54 @@ FALSE=0
 
 result=0
 
-RUNNING=$TRUE
-while (($RUNNING == $TRUE)); do 
+running=$TRUE
+while (($running == $TRUE)); do #Run the program until the variable $running is changed
 
-	echo -n 'Please insert a number: ' 
-	read numOne #Store first number in variable numOne
+	error=$TRUE # Ask for a number until the user gives one
+	while (($error == $TRUE)); do
+		echo -n 'Please insert a number: ' 
+		read numOne #Store first number in variable numOne
+		if [ "$numOne" == ":q" ]; then
+			echo Thanks for using my calculator
+			exit 
+		fi
+		if [ "$numOne" -eq "$numOne"  ] 2>/dev/null; then
+			#Its an integer
+			error=$FALSE
+		else
+			#Its not an integer
+			echo -n "ERROR! "
+		fi
+	done
+	error=$TRUE
 
-	echo -n 'Please insert an operation: '
-	read opOne #Store operation into variable opOne
+	while (($error == $TRUE)); do
+		echo -n 'Please insert an operation: '
+		read opOne #Store operation into variable opOne
 
-	echo $numOne $opOne
-	RUNNING=$FALSE
+		case "$opOne" in
+		
+		"+") result=$(($result + $numOne))
+		     error=$FALSE
+		     ;;
+		"-") result=$(($result - $numOne))
+		     error=$FALSE
+		     ;;
+		"*") result=$(($result * $numOne))
+		     error=$FALSE
+		     ;;
+		"/") result=$(($result / $numOne))
+		     error=$FALSE
+	 	     ;;
+	       ":q") error=$FALSE
+		     echo "Thanks for using my calculator"
+		     exit
+		     ;;
+		*  ) echo -n "ERROR! "
+		     ;;
+		esac
+	done
+	echo "Result so far is: $result"
 done
 
 
