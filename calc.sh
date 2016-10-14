f674011e3f2ccd1cf30eb7dc5c38805e077fa4ae
LukePaddock <14lp9@queensu.ca>
2016-10-14 15:20:33 -0400

    Got the master loop owrking

--- calc.sh
+++ calc.sh
@@ -3,4 +3,22 @@
 #	    ID: 10157251
 #	NET ID: 14lp9
 
-echo Hello World!
+TRUE=1
+FALSE=0
+
+result=0
+
+RUNNING=$TRUE
+while (($RUNNING == $TRUE)); do 
+
+	echo -n 'Please insert a number: ' 
+	read numOne #Store first number in variable numOne
+
+	echo -n 'Please insert an operation: '
+	read opOne #Store operation into variable opOne
+
+	echo $numOne $opOne
+	RUNNING=$FALSE
+done
+
+
